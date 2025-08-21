f31493330fc9cfb207e759bcc96a7d695ef466a9
tuliodev <tuliorsc10@gmail.com>
2025-08-20 22:16:11 -0300

    Implementation client's endpoints

--- src/main/java/com/tulio/store_multimarcas/client/controller/ClientController.java
+++ src/main/java/com/tulio/store_multimarcas/client/controller/ClientController.java
@@ -1,9 +1,7 @@
 package com.tulio.store_multimarcas.client.controller;
 
-import com.tulio.store_multimarcas.client.dto.CreateClientReponseDTO;
-import com.tulio.store_multimarcas.client.dto.CreateClientRequestDTO;
+import com.tulio.store_multimarcas.client.dto.*;
 import com.tulio.store_multimarcas.client.service.ClientService;
-import com.tulio.store_multimarcas.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +15,24 @@ public class ClientController {
     private final ClientService clientService;
 
     @PostMapping()
-    public ResponseEntity<CreateClientReponseDTO> createUser(@RequestBody @Valid CreateClientRequestDTO request) {
+    public ResponseEntity<CreateClientReponseDTO> createClient(@RequestBody @Valid CreateClientRequestDTO request) {
         return ResponseEntity.ok(clientService.createClient(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RetrieveClientDTO> retrieveClientById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.retrieveClientById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClientById(@PathVariable Long id) {
+        clientService.deleteClientById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UpdateClientResponseDTO> updateClient(@PathVariable Long id, @RequestBody @Valid UpdateClientResquestDTO request) {
+        return ResponseEntity.ok(clientService.updateClient(id, request));
     }
 }
 
